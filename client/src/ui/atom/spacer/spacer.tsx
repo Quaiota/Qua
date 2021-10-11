5c45ef8fb238a535b17710f63a70474b267006ab
Roosevelt innocent <howdy.roosevelt@gmail.com>
2021-10-11 16:29:00 +0100

    Fix home and onboarding ui

--- client/src/ui/atom/spacer/spacer.tsx
+++ client/src/ui/atom/spacer/spacer.tsx
@@ -1,13 +1,13 @@
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'
 
 interface ISpacer {
-  horizontalSpace?: boolean;
-  size: string | number;
+  horizontalSpace?: boolean
+  size: string | number
 }
 
 const Spacer = styled.div<ISpacer>`
   padding-left: ${(props) => props.horizontalSpace && props.size};
   display: ${(props) => (props.horizontalSpace ? 'inline-block' : 'block')};
   padding-top: ${(props) => !props.horizontalSpace && props.size};
-`;
-export default Spacer;
+`
+export default Spacer
