d8c3337ff9eac427bfeb1fe12ff4407a23495b75
giprim <gi4real.gi@gmail.com>
2021-10-12 21:26:46 +0100

    feat: checkbox and social onboarding design

--- client/src/pages/ConstructionSite.tsx
+++ client/src/pages/ConstructionSite.tsx
@@ -1,62 +1,87 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
-import Spacer from '../ui/atom/spacer/spacer';
+import Button from '../ui/atom/button/Button';
 import TextWrap from '../ui/atom/typography/TextWrap';
-import PostHeader from '../ui/molecules/post/PostHeader';
-import ProfileHeader from '../ui/molecules/profileHeader/ProfileHeader';
-import SocialSideNav from '../ui/molecules/socialNav/SocialSideNav';
-import SocialTopNav from '../ui/molecules/socialNav/SocialTopNav';
-import Modal from '../ui/organisms/modal/Modal';
+import CheckBox from '../ui/molecules/checkBox/CheckBox';
 
 const ConstructionSite = () => {
-  const [show, setShow] = useState(false);
-
-  const toggleModal = () => {
-    setShow(!show);
-  };
   return (
-    <StyleContent>
-      {/* <button onClick={toggleModal}>open modal</button> */}
-
-      <SocialTopNav />
-      <Spacer size='3rem' />
-      <SocialSideNav />
-
-      <Spacer size='3rem' />
-      <div className='box'>
-        <ProfileHeader size='large' />
-        <Spacer size='2rem' />
-        <Spacer size='2rem' />
-        <div>
-          <ProfileHeader />
-          <PostHeader />
-          <Spacer size='1rem' />
-          <TextWrap fontSize='body'>
-            We need to move NFTs onto the layer 2 ecosystem to cut fees.
-            However, doing that *right* requires good cross-rollup portability
-            standards, so the ecosystem can avoid getting locked into one
-            particular L2. The NFT ecosystem is growing rapidly, and it’s a
-            significant part of the Ethereum chain’s gas consumption.
+    <SocialOnboard>
+      <form className='wrapper'>
+        <section className='pager'>
+          <TextWrap fontSize='bodymd' bold='bold'>
+            Onboarding
           </TextWrap>
+          <TextWrap fontSize='bodymd' bold='bold'>
+            1/1
+          </TextWrap>
+        </section>
+        <div className='caption-box'>
+          <TextWrap fontSize='h2'>Topic of Interests</TextWrap>
+          <div className='sub-text'>
+            <TextWrap fontSize='bodymd'>
+              Add the topics that matter most to you, <br /> You can edit your
+              topic of interest anytime.
+            </TextWrap>
+          </div>
         </div>
-      </div>
-
-      <Modal open={show} close={toggleModal}>
-        <div className='display'>
-          <h1>Hello world</h1>
+        <section className='checklist'>
+          <CheckBox label='Academia' name='academia' />
+          <CheckBox label='Arts & culture' name='arts' />
+          <CheckBox label='Business & finance' name='business' />
+          <CheckBox label='Careers' name='careers' />
+          <CheckBox label='Design' name='design' />
+          <CheckBox label='Entertainment' name='entertainment' />
+          <CheckBox label='Fashion & beauty' name='fashionbeauty' />
+          <CheckBox label='Fitness' name='Fitness' />
+        </section>
+        <div className='submit-box'>
+          <Button type='submit' btnType='solid'>
+            Complete
+          </Button>
         </div>
-      </Modal>
-    </StyleContent>
+      </form>
+    </SocialOnboard>
   );
 };
 
-const StyleContent = styled.div`
-  background: ${({ theme }) => theme.black.matteblack};
-  min-height: 90vh;
-  background: gray;
-  .box {
-    max-width: 540px;
+const SocialOnboard = styled.div`
+  height: 100vh;
+  overflow-y: auto;
+  background: ${({ theme }) => theme.accent.purple};
+  .wrapper {
+    max-width: 600px;
+    padding: 1rem;
     margin: 0 auto;
+
+    .pager {
+      display: flex;
+      justify-content: space-between;
+      padding-block: 12px;
+    }
+    .checklist {
+      padding-block: 1rem;
+    }
+    .submit-box {
+      max-width: 340px;
+      margin: 0 auto;
+      padding-top: 1rem;
+      button {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+      }
+    }
+    .caption-box {
+      padding-top: 12px;
+      .sub-text {
+        padding-top: 8px;
+      }
+    }
+
+    .pager,
+    .caption-box {
+      padding-inline: 1rem;
+    }
   }
 `;
 
---
+++ client/src/ui/molecules/checkBox/CheckBox.tsx
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled';
+import breakpoint from '../../configs/breakpoint';
+
+interface ICheckBox {
+  label?: string;
+  name: string;
+}
+
+const CheckBox: React.FC<ICheckBox> = ({ name, label }) => {
+  return (
+    <StyledCheckItem>
+      <input id={name} type='checkbox' />
+      <label htmlFor={name}>{label}</label>
+    </StyledCheckItem>
+  );
+};
+
+const StyledCheckItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  label {
+    cursor: pointer;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+    font-style: normal;
+    font-weight: bold;
+    padding: 1rem;
+    font-size: 16px;
+    line-height: 26px;
+    color: ${({ theme }) => theme.primary.white};
+    transition: all 0.41s ease-in;
+    border-radius: 8px;
+    :hover {
+      background: ${({ theme }) => theme.primary.white + '12'};
+    }
+    &::before {
+      transition: all 0.41s ease-in;
+      content: '';
+      width: 1em;
+      height: 1em;
+      border-radius: 0.15em;
+      border: 2px solid ${({ theme }) => theme.primary.white};
+    }
+    @media (min-width: ${breakpoint.tab}px) {
+      font-size: 18px;
+    }
+  }
+  input[type='checkbox'] {
+    cursor: pointer;
+    opacity: 0;
+    position: absolute;
+
+    :checked + label::before {
+      content: '\\2713';
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      background: ${({ theme }) => theme.primary.white};
+      color: ${({ theme }) => theme.accent.purple};
+    }
+  }
+`;
+
+export default CheckBox;
