5ef0404951d2c0f03308678b1f312d6d22fce3c2
Roosevelt innocent <howdy.roosevelt@gmail.com>
2021-10-17 07:37:15 +0100

    litepaper

--- client/src/pages/litepaper/litepaper.ts
+++ client/src/pages/litepaper/litepaper.ts
@@ -1,11 +1,92 @@
 const litepaper = `
 
-# ADD YOUR LITEPAPER WRITE UP  HERE \n\r
 \n\r
+# Quaiota
+\n\r
+
+System for building wealth for digital people.
+
+**Litepaper**
 \n
+August 15th, 2021.
 
 
-- we win again
-`;
+# Introduction
 
-export default litepaper;
+The advent of blockchain and cryptocurrency has been a massive breakthrough that has opened up many unique opportunities for digital people. 
+
+For the first time in internet history, everyone can now participate in a fair, trustless, and richly incentivized system on the internet.
+
+This new trend of how things are supposed to work has sprung up several types of projects that are defining the future of what it means to build a truly participatory internet, and Quaiota is one of several projects that aims to solve this problem.
+
+The Quaiota is a decentralized, permissionless network that solves identity and utility as a problem we experience on web3 today.
+
+## Current State of web3 apps
+
+Our experience so far with apps existing on the web3 space is the fact that they are isolated from one another, and most of the apps that exist provide very little or no value to consumers
+
+We conducted an internet survey and what we learned was that a lot of people aren't aware of what web3 is, found it a bit challenging to use, or didn't find any real value attached to the apps built on web3.
+
+## Current Issues with web3 identity
+
+We think one other problem facing web3 today is the reality that anyone can have multiple addresses or identities that enables them to interact with web3 apps, although these addresses or identities can be difficult to keep track of or honor for people existing in a social system.ther by clicking a file in the tree.
+
+## The Quaiota difference
+
+The Quaiota will be the first protocol that provides a seamless bridge between developers and consumers in the web3 space. It will make the onboarding process for users coming into web3 to interact with apps simple and easy, and for developers, it removes the hassle of authenticating users on their web3 apps.
+
+**Identity**
+The Quaiota enables every user to claim their identity as a non-fungible token stored on the blockchain.
+
+Each identity is unique, verifiable and can vote and participate in several apps and systems across Quaiota.
+
+Identifiable profiles can link one of their public addresses to their human-readable non-fungible QUA ID.
+
+**Utilities**
+Utilities are referred to: as apps and systems on the Quaiota. Every application on the quaiota features a system that enables several long-term reward mechanisms for people participating in the network.
+
+The Quaiota will enable users to connect and switch from different web3 applications all in one place, with one unique identity.
+
+## How Does Quaiota Work?
+
+**One identity. Multiple utilities.**
+By claiming your identity as an NFT on the Quaiota, you can discover and connect to any web3 app almost immediately.
+
+**SDK for developers**
+The Quaiota provides an easy-to-use SDK that is flexible for developers to architect incentives on their web3 apps.
+
+**PWA wherever you are**
+The Quaiota network lives on the web. Anyone from anywhere can access it as a progressive web app (PWA).
+
+**Smart contracts code as laws**
+Every decision and incentive is implemented by the smart contract that a developer specified for their app.
+
+## QUA Tokens - $QUA
+
+**$QUA** tokens are the native token of the Qua ecosystem. Holders of the token can vote, participate in activities, and architect systems across Quaiota.
+
+$QUA will possess the following utility:
+
+-   Voting: $QUA tokens represent voting rights to participate in DAO decisions, and play a significant role in Qua ecosystem governance.
+
+-   Trading: $QUA is the default token used to perform transactions across the Quaiota
+
+-   Economics: $QUA tokens will be responsible for built-in incentives and reward mechanisms architected by different apps on the Quaiota.
+
+## Qua DAO
+
+The Qua DAO is the main governing body of the Qua network. $QUA token holders can participate and vote on community proposals and amendments. Community votes include but are not limited to community incentives and network upgrades.
+  
+## Token Economics
+
+![Tokenomics](https://lh3.googleusercontent.com/3yu1wZjWZ9ZVwXA3qQOFpCsKw9AfzwEX-Hd1tzf06CcJo0UlsZc-cfXd6y0bAloTKsLmrQAO5XQ1yxwGQu_GgBHJcjP5-3UrkEMa0mL41MuxfaJCka88VIbnAklZWf46XtU7OR28Jg=w2400)
+
+
+
+
+
+
+
+`
+
+export default litepaper
