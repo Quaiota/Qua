85c48a8494e8885e772ded9443212ed323d81d2b
Roosevelt innocent <howdy.roosevelt@gmail.com>
2021-10-19 13:03:06 +0100

    update website

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,91 +1,99 @@
-import { useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
-import { motion } from 'framer-motion';
-import { memo, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../ui/atom/button/Button';
-import Logo from '../ui/atom/logo/Logo';
-import Spacer from '../ui/atom/spacer/spacer';
-import TextWrap from '../ui/atom/typography/TextWrap';
-import breakpoint from '../ui/configs/breakpoint';
-import Footer from '../ui/organisms/footer/Footer';
-import OnBoard from './onboarding';
+import { useTheme } from '@emotion/react'
+import styled from '@emotion/styled'
+import { motion } from 'framer-motion'
+import { memo, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Feed from '../assets/images/feed.png'
+import Profile from '../assets/images/profile.png'
+import Button from '../ui/atom/button/Button'
+import Logo from '../ui/atom/logo/Logo'
+import Spacer from '../ui/atom/spacer/spacer'
+import TextWrap from '../ui/atom/typography/TextWrap'
+import breakpoint from '../ui/configs/breakpoint'
+import Footer from '../ui/organisms/footer/Footer'
+import OnBoard from './onboarding'
 
 const Home = memo(() => {
-  const color = useTheme();
-  const [show, setShow] = useState(false);
+  const color = useTheme()
+  const [show, setShow] = useState(false)
   const toggleModal = () => {
-    setShow(!show);
-  };
+    setShow(!show)
+  }
 
   return (
     <MainBox>
-      <Link to='/' className='logoHolder'>
-        <Logo size='lg' />
+      <Link to="/" className="logoHolder">
+        <Logo size="lg" />
       </Link>
       <StyledFlexBox>
-        <div className='box'>
-          <div className='wrapper'>
-            <TextWrap fontSize='lg' bold='bolder'>
+        <div className="box">
+          <div className="wrapper">
+            <TextWrap fontSize="lg" bold="bolder">
               Quaiota
             </TextWrap>
 
-            <Spacer size='1.2rem' />
-            <TextWrap fontSize='body'>
+            <Spacer size="1.2rem" />
+            <TextWrap fontSize="body">
               Your decentralized web3 profile where you can access apps and
               systems that enables everyone to build wealth on the internet
             </TextWrap>
-            <Spacer size='1.5rem' />
-            <TextWrap fontSize='body'>
+            <Spacer size="1.5rem" />
+            <TextWrap fontSize="body">
               Built on the Etheruem Blockchain.
             </TextWrap>
 
             <section>
               <Button
-                btnType='solid'
-                bold='bold'
+                btnType="solid"
+                bold="bold"
                 onClick={toggleModal}
                 bgColor={color.accent.cyan}
-                textColor={color.black.matteblack}>
+                textColor={color.black.matteblack}
+              >
                 Launch app
               </Button>
-              <Link to='/litepaper' className='link'>
+              <Link to="/litepaper" className="link">
                 Learn more
               </Link>
             </section>
           </div>
         </div>
         <StyledColoredBox color={color.primary.mint1}>
-          <div></div>
+          <div>
+            <img src={Profile} alt="Profile Dashboard" />
+          </div>
         </StyledColoredBox>
       </StyledFlexBox>
 
       <StyledFlexBox>
         <StyledColoredBox color={color.primary.pop1}>
-          <div></div>
+          <div>
+            <img src={Feed} alt="Social Feed" />
+          </div>
         </StyledColoredBox>
-        <div className='box'>
-          <div className='wrapper'>
-            <TextWrap fontSize='lg' bold='bolder'>
+        <div className="box">
+          <div className="wrapper">
+            <TextWrap fontSize="lg" bold="bolder">
               Social
             </TextWrap>
 
-            <Spacer size='1.2rem' />
-            <TextWrap fontSize='body'>
+            <Spacer size="1.2rem" />
+            <TextWrap fontSize="body">
               Quality social network on the Quaiota focused on decentralized
               media and academia.
             </TextWrap>
 
             <section>
               <Button
-                btnType='solid'
-                bold='bold'
+                btnType="solid"
+                bold="bold"
                 onClick={toggleModal}
                 bgColor={color.primary.hot1}
-                textColor={color.black.matteblack}>
+                textColor={color.black.matteblack}
+              >
                 Join
               </Button>
-              <Link to='/' className='link'>
+              <Link to="/" className="link">
                 How it works
               </Link>
             </section>
@@ -93,24 +101,28 @@ const Home = memo(() => {
         </div>
       </StyledFlexBox>
       <StyledBuiltForPeople>
-        <div className='wrapper'>
-          <TextWrap fontSize='lg' bold='bolder'>
-            Built for the People
-          </TextWrap>
-          <TextWrap>Description</TextWrap>
-
-          <div className='flexbox'>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="wrapper">
+          <h3>Built for Digital People</h3>
+          <div className="flexbox">
+            <div>
+              <Link to="/litepaper" className="link">
+                Read the Quaiota litepaper
+              </Link>
+            </div>
+            <div onClick={toggleModal}>
+              <p>Get the $QUA Token</p>
+            </div>
+            <div onClick={toggleModal}>
+              <p>Claim your NFT Quaiota ID</p>
+            </div>
           </div>
         </div>
       </StyledBuiltForPeople>
       <Footer />
       {show && <OnBoard show={show} toggleModal={toggleModal} />}
     </MainBox>
-  );
-});
+  )
+})
 
 const StyledBuiltForPeople = styled.section`
   padding-block: 12%;
@@ -118,6 +130,15 @@ const StyledBuiltForPeople = styled.section`
   .wrapper {
     max-width: 100%;
     margin: 0 auto;
+    & h3 {
+      font-size: 48px;
+      font-family: 'Darker Grotesque';
+      color: ${({ theme }) => theme.primary.sea3};
+
+      @media (max-width: ${breakpoint.mobile}px) {
+        font-size: 36px;
+      }
+    }
     .flexbox {
       display: flex;
       flex-wrap: wrap;
@@ -126,12 +147,37 @@ const StyledBuiltForPeople = styled.section`
       padding-block: 1rem;
       flex-direction: column;
       align-items: center;
+      margin-top: 2rem;
+
+      @media (min-width: ${breakpoint.desktop}px) {
+        height: 280px;
+      }
+      @media (max-width: ${breakpoint.mobile}px) {
+        row-gap: 1rem;
+      }
+
       div {
         max-width: 340px;
+        height: 100%;
         width: 100%;
-        min-height: 340px;
         background: ${({ theme }) => theme.primary.pop1};
         border-radius: 24px;
+        font-size: 28px;
+        font-weight: bold;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 2rem;
+
+        @media (max-width: ${breakpoint.mobile}px) {
+          padding: 3rem 2rem;
+        }
+
+        & a {
+          color: ${({ theme }) => theme.black.matteblack};
+          text-decoration: none;
+        }
         :first-child {
           background: ${({ theme }) => theme.primary.hot1};
         }
@@ -151,7 +197,7 @@ const StyledBuiltForPeople = styled.section`
       }
     }
   }
-`;
+`
 
 const MainBox = styled.div`
   width: 100%;
@@ -204,33 +250,42 @@ const MainBox = styled.div`
       padding-block: 10%;
     }
   }
-`;
+`
 
 const StyledColoredBox = styled(motion.div)`
   background: ${(props) => props.color ?? ''};
   min-height: 50vh;
   display: inline-flex;
+  align-items: center;
   justify-content: center;
   padding: 1rem;
   padding-bottom: 0;
   width: 100%;
   position: relative;
+
+  & img {
+    @media (min-width: ${breakpoint.desktop}px) {
+      width: 375px;
+    }
+    @media (max-width: ${breakpoint.mobile}px) {
+      width: 100%;
+    }
+  }
   div {
-    position: absolute;
     height: 100%;
-    bottom: 0;
-    max-height: 60%;
     max-width: 370px;
     width: 100%;
     border-radius: 36px 36px 0 0;
     margin-top: 4%;
-    background: ${({ theme }) => theme.black.matteblack};
 
     @media (min-width: ${breakpoint.tab}px) {
       margin-top: 6%;
     }
+    @media (min-width: ${breakpoint.desktop}px) {
+      max-height: 80%;
+    }
   }
-`;
+`
 
 const StyledFlexBox = styled.div`
   display: flex;
@@ -240,6 +295,6 @@ const StyledFlexBox = styled.div`
     margin-bottom: 2.5rem;
     flex-wrap: nowrap;
   }
-`;
+`
 
-export default Home;
+export default Home
